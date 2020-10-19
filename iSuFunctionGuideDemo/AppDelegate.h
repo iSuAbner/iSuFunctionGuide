6fc671a0bf8c47f2211498f250933c9c86355078
巴文辉 <bawenhui@pwrd.com>
2020-10-19 15:14:13 +0800

    build: refresh data that has been uploaded

---
+++ iSuFunctionGuideDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSuFunctionGuideDemo
+//
+//  Created by 巴文辉 on 2020/9/29.
+//  Copyright © 2020 巴文辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
